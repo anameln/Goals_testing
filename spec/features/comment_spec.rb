6f3d9c5c6215d0294b146faf39687112b5d599d0
Dean+Katrina <dean+katrina@appacademy.io>
2015-06-22 15:04:41 -0400

    Add specs for user and goal comments

--- spec/features/comment_spec.rb
+++ spec/features/comment_spec.rb
@@ -20,7 +20,31 @@ feature "user comments" do
     click_button "Add Comment"
     expect(page).to have_content "Hey it's me!"
   end
+end
 
+feature "goal comments" do
+  before :each do
+    sign_up_as_sennacy
+    fill_in 'Goal', with: 'Pass the rails assessment'
+    choose('Public')
+    click_button 'Add Goal'
+    sign_out
+  end
 
+  it "can't comment on goals unless signed in" do
+    click_link('Pass the rails assessment')
+    expect(page).to have_content "Sign In"
+  end
+
+  it "redirects to user's page after making a comment" do
+    sign_up("katrina", "hurricane")
+    visit "/users/1"
+    click_link('Pass the rails assessment')
+    fill_in 'Comment', with: "Gonna study hard tonight!"
+    click_button 'Add Comment'
+    expect(page).to have_content "Gonna study hard tonight!"
+    expect(page).to have_content "Pass the rails assessment"
+    expect(page).to have_content "Sennacy"
+  end
 
 end
